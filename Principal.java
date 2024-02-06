4215070b17d28b8f20fb23f55dcba85718991022
Alejandra Agudelo varela <339210@alu.murciaeduca.es>
2024-02-06 18:10:09 +0100

    Añadido Principal que genera Hola Mundo

---
+++ Principal.java
@@ -0,0 +1,7 @@
+public class Principal{
+	public static void main (Strings[]arvs){
+
+		System.out.println("Hola Mundo);
+
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Practica4.1
+## Repositorio para la ealización de la Practica4.1
